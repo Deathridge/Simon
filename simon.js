70517af318f529c1e652da753884f60c01d2569c
Deathridge <danielrbetteridge@gmail.com>
2016-04-18 16:27:17 +0800

    first commit

---
+++ simon.js
@@ -0,0 +1,103 @@
+var sequence = [];
+var sequenceLength = 5;
+var sounds = ['https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3', 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3'];
+var soundsPlayers = [new Audio(sounds[0]), new Audio(sounds[1]), new Audio(sounds[2]), new Audio(sounds[3])];
+var sadTrombone = new Audio('http://www.freesound.org/data/previews/73/73581_634166-lq.mp3');
+var strict = false;
+newGame();
+
+var selections = [];
+var count = 1;
+
+function beginGame(){
+	document.getElementById('start').onclick = beginGame;
+
+	count = 1
+	addLight(0);
+}
+
+function addLight(buttonLoc){
+	soundsPlayers[sequence[buttonLoc]-1].play();
+	document.getElementById(sequence[buttonLoc]).className += ' light';
+	setTimeout(removeLight, 500,buttonLoc);
+}
+
+function removeLight(buttonLoc){
+
+	var clickable = document.getElementsByClassName('clickable')
+	for(var j=0;j<clickable.length;j++){
+		clickable[j].onclick = sumButtons;
+	}
+	document.getElementById(sequence[buttonLoc]).classList.remove("light");
+	if(buttonLoc+1 < count){
+
+		setTimeout(addLight,500,buttonLoc+1);
+	}
+}
+
+function sumButtons(){
+	soundsPlayers[this.id-1].play();
+	selections.push(this.id);
+	console.log(selections);
+	if(selections.length == count){
+		if(isSolved(sequence, count, selections) && !isWinner(sequence, selections) && count < sequence.length){
+			count++;
+			document.getElementById('counter').innerHTML = count;
+			selections = [];
+			setTimeout(addLight, 1000, 0);
+		}else if(isWinner(sequence, selections)){
+			window.alert("Winner!");
+			newGame();
+		}else {
+			setTimeout(addLight,500, 0);
+			
+		}
+	}
+
+	for(var i=0;i<selections.length;i++){
+		if(sequence[i] != selections[i] && !strict){
+			sadTrombone.play();
+			selections = [];
+			setTimeout(addLight,2000, 0);
+		}else if(sequence[i] != selections[i] && strict){
+			sadTrombone.play();
+			setTimeout(newGame, 2000);
+		}
+	}		
+	 
+}
+
+function isSolved(sequence, countMax, selections){
+	for(var i=0;i<countMax;i++){
+		if(sequence[i] != selections[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+function isWinner(sequence, selections){
+	for(var i=0;i<sequence.length;i++){
+		if(sequence[i] != selections[i]){
+			return false;
+		}
+	}
+	return true;
+}
+
+function newGame(){
+	document.getElementById('strict').onclick = function(){ strict = true;}
+	sequence = [];
+	selections = [];
+	count = 1;
+	document.getElementById('counter').innerHTML = count;
+	var sequenceMember;
+	for (var i=0;i< sequenceLength;i++){
+		sequenceMember = Math.ceil(Math.random() * 4);
+		while(sequenceMember == sequence[sequence.length-1]){
+			sequenceMember = Math.ceil(Math.random() * 4)			
+		}
+		sequence.push(sequenceMember);
+	}
+	document.getElementById('start').onclick = beginGame;
+}
